a1d4b272ffa984f53fea6792473ca5dbf6234add
Shimgo2008 <132872880+Shimgo2008@users.noreply.github.com>
2024-08-06 18:56:28 +0900

    toUP.jsを追加
    サイトスクロールを10%以上した時、favicon.pngを半透明で表示、favicon.pngをクリックした場合サイト上部へ移動
    htmlにscriptタグ追加していないため、userにはわからないが、動作テストは問題なく動いた

---
+++ js/toUp.js
@@ -0,0 +1,86 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const styleTag = document.createElement('style');
+    const linkTag = document.createElement('link');
+    const divTag = document.createElement('div');
+    const aTag = document.createElement('a');
+    const imgTag = document.createElement('img');
+
+    linkTag.rel = "stylesheet";
+    linkTag.href = "https://use.fontawesome.com/releases/v5.6.4/css/all.css";
+
+    styleTag.textContent = `
+    html{
+      scroll-behavior: smooth;
+    }
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 0.6;
+      }
+    }
+
+    #page_top {
+        width: 50px;
+        height: 50px;
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        opacity: 0;
+    }
+
+    #page_top img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        transform: scale(1.5); /* 150%に拡大 */
+        transform-origin: right; /* 中心を基準に拡大 */
+    }
+
+    #page_top.show {
+      opacity: 1; 
+    }
+    #page_top a{
+      position: relative;
+      display: block;
+      width: 50px;
+      height: 50px;
+      text-decoration: none;
+    }
+    `;
+
+    divTag.id = 'page_top';
+
+    aTag.href = '#';
+
+    imgTag.src = '../img/favicon.png';
+
+    document.head.appendChild(styleTag);
+    console.log("styleTag 追加", styleTag);
+    document.head.appendChild(linkTag);
+    console.log("linkTag 追加", linkTag);
+    aTag.appendChild(imgTag);
+    console.log("imgTag 追加", imgTag);
+    divTag.appendChild(aTag);
+    console.log("aTag 追加", aTag);
+    document.body.appendChild(divTag);
+    console.log("divTag 追加", divTag);
+
+    imgTag.onload = function() {
+        console.log("画像の幅: " + imgTag.width + "px");
+        console.log("画像の高さ: " + imgTag.height + "px");
+    };
+
+    window.addEventListener('scroll', function() {
+        const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+        const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const scrollPercentage = (scrollPosition / windowHeight) * 100;
+
+        if (scrollPercentage > 10) {
+            divTag.classList.add('show');
+        } else {
+            divTag.classList.remove('show');
+        }
+    });
+});
